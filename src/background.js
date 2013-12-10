b9467a8943bffb67723899189e359be129bdc977
Your full name <remco@red5.nl>
2013-12-10 15:32:06 +0100

    cleanup

--- src/background.js
+++ src/background.js
@@ -29,62 +29,6 @@ chrome.extension.onMessage.addListener( function(request, sender, sendResponse) 
 	    loadConfig();	    
 	    sendResponse({});
 	} else {
-	    var extensions = {
-		    'http://bitcoinwisdom.com/.*': {
-			title: 'test',
-			description: '',
-			author: '',
-			version: "0.0.1",
-			website: 'http:///',
-			css: [
-			    "a.link_premium { display: none; } .gg160x600 { display: none; }"
-			],
-			script: [
-			    "/* console.debug(document.readyState); $(document).ready(function() {$( window ).trigger('resize'); console.debug('remco'); }); */"
-			]
-		    },
-		    'https://www.google.nl/.*': {
-			css: [
-			    ".ads-container { display: none !important; }; "
-			],
-			script: [
-			    
-			]
-		    },
-		    'http://kickass.to/.*': {
-			css: [
-			    ".advertising, .slotbox, .tabs { display: none !important; }; "
-			],
-			script: [
-			    
-			]
-		    },
-		    'http://www.(zie|nu).nl/.*': {
-			css: [
-			    "#pageheader { min-height: 30px !important;} .adblock_h, #adblock_v, .adblock_noborder { display: none !important; }"
-			],
-			script: [
-			    /* remove ga.js and twitter */
-			]
-		    },
-		    'https://btc-e.com/.*': {
-			css: [
-			    "#content div:first-child .block:first-child {display: none !important;} p.gray { font-weight: bold; }; "
-			],
-			script: [
-			    /* remove ga.js and twitter */
-			]
-		    }, 
-		    'https://twitter.com/.*': {
-			css: [
-			    "body { background-image: none !important; background-color: #fff !important; }; "
-			],
-			script: [
-			    
-			]
-		    }
-	    }
-	    
 	    chrome.browserAction.disable(sender.tab.id);
 	    chrome.browserAction.setBadgeText({ text:'', tabId: sender.tab.id});
 	    chrome.browserAction.setBadgeBackgroundColor({ color:'#FFF', tabId: sender.tab.id } );
